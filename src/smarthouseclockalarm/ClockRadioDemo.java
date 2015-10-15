4842b1d6f8627a67822ab8c1627f2a0fdfe6b402
Matt <Matt@152.13.52.186>
2015-10-15 11:36:05 -0400

    new Commit 10/15

---
+++ src/smarthouseclockalarm/ClockRadioDemo.java
@@ -0,0 +1,27 @@
+
+package smarthouseclockalarm;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Matt
+ */
+public class ClockRadioDemo extends JFrame{
+
+    
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ClockRadioKeyPad key = new ClockRadioKeyPad();
+        //ClockRadioKeyPad key1 = new ClockRadioKeyPad();
+        //ClockRadioGUI GUI = new ClockRadioGUI();
+      //ClockRadioKeyPad key = new ClockRadioKeyPad();
+       // new DigitalClock();
+    }
+}
